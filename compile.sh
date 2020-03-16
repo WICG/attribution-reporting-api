3ad9138887fefe9b1765bdd45b61ae40a0607553
Charlie Harrison <csharrison@chromium.org>
2020-03-16 16:40:39 -0500

    Add error handling for non-200 http status codes from bikeshed

--- compile.sh
+++ compile.sh
@@ -7,10 +7,16 @@ set -x
 set -e
 
 # Run bikeshed.  If there are errors, exit with a non-zero code
-curl https://api.csswg.org/bikeshed/ -F file=@index.bs -F force=1 > index.html;
+HTTP_RESPONSE=$(curl -s -o index.html -w "%{http_code}" https://api.csswg.org/bikeshed/ -F file=@index.bs -F force=1)
 
 # The out directory should contain everything needed to produce the
 # HTML version of the spec.
+if [ $HTTP_RESPONSE -eq "200" ]; then
+  mkdir -p out
+  mv index.html out
+else
+  cat index.html
+  rm -f index.html
+  exit 22
+fi
 
-mkdir -p out
-mv index.html out
