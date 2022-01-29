ce86650a37ba96aa18837ef92302a92be04117b6
Charlie Harrison <csharrison@chromium.org>
2022-01-29 09:46:35 -0600

    denoise

--- noise_corrector.py
+++ noise_corrector.py
@@ -3,68 +3,70 @@
 import collections
 import random
 
-# This file provides a helper function to correct noisy grous of reports coming
-# from the event level API (i.e. those where conversion metadata is potentially
-# randomly selected). It implements an unbiased estimator for the true counts
-# of conversions for each metadata bucket.
+# This file provides a helper function to correct noisy data coming from the
+# Attribution Reporting API.
 
-# TODO(csharrison): This estimator is unbiased, but its variance can be
-# improved if we allow bias. This is something we should be able to smoothly
-# trade off with another parameter.
-def corrected_buckets(buckets, noise_probability=.05):
-  """Returns a map of conversion bits --> corrected counts
+# Estimates the true values for data generated through k-ary randomized
+# response. It includes a parameter `beta` which allows smoothly trading off
+# bias (minimized with `beta` = 0) and variance (minimized when `beta` = 1).
+def estimate_true_values(observed_values, randomized_response_rate, beta=0):
+  """Returns a list of corrected counts
 
-  buckets: A map from integer conversion metadata to conversion counts.
-       note, this needs to include buckets with 0 counts.
-  noise_probability: The probability the metadata was randomly selected
+  observed_values: A list of size `k` for k-ary randomized response, where the
+    ith value in the list indicates the counts of the ith output (out of k).
+  randomized_response_rate: The probability any given output is randomized
   """
+  n = sum(observed_values)
+  r = 1./len(observed_values)
+  x = randomized_response_rate
 
-  total_records = sum(buckets.values())
-  num_conversion_buckets = len(buckets)
-
-  # |noise_probability| of the reports are noised and uniformly distributed
-  # among the conversion buckets so one can calculate how many values have
-  # were from noised, per bucket.
-  noised_values_per_bucket = total_records * noise_probability / num_conversion_buckets
-
-  # Subtract the reports added to each bucket due to noise, and rescale to
-  # account for the reports that were shifted due to the initial noise.
-  corrected_buckets = {
-      bucket: (v - noised_values_per_bucket) / (1 - noise_probability)
-      for bucket, v in buckets.items()
-  }
-  return corrected_buckets
+  # An observed value v will be distributed according to
+  # BinomialDistribution[n, q] where
+  # q = (1 - x)*(v' / n) + xr
+  # Where v' is the true value.
+  #
+  # E[v_hat] = E[(v - (1 - b)nxr) / (1 - (1 - b)x)]
+  # = (v'(1 - x) + nxr - (1 - b)nxr) / (1 - (1 - b)x)
+  # = (v'(1 - x) + bnxr) / (1 - (1 - b)x)
+  #
+  # For b = 0 --> E[v_hat] = v' i.e. the estimator is unbiased.
+  # For b = 1 --> E[v_hat] = v'(1-x) + bnxr
+  #
+  # V[v_hat] = V[(v - (1 - b)nxr) / (1 - (1 - b)x)]
+  # = V[v] / (1 - (1 - b)x)^2
+  # = nq(1-q) / (1 - (1 - b)x)^2
+  #
+  # For b = 0 --> V[v_hat] = nq(1-q) / (1 - x)^2
+  # For b = 1 --> V[v_hat] = nq(1-q)
+  def estimate(v):
+    return (v - (1 - beta)*n*x*r) / (1 - (1 - beta)*x)
+  return map(estimate, observed_values)
 
 if __name__ == "__main__":
-  # The following is an example showing how to use the function.
-  # |example_reports| is a map from bucket --> count of conversions
-  # with that bucket.
-  example_reports = {
-      0: 50,
-      1: 150,
-      2: 300,
-      3: 400,
-      4: 700,
-      5: 200,
-      6: 0,
-      7: 2000
-  }
+  # TODO(csharrison): Make the example robust to more parameter levels.
+  # This example assumes 1 bit of metadata, 1 trigger per source, and 1
+  # reporting window.
+  true_reports = [
+      50000, # Sources with no reports
+      1000,  # Sources with conversion metadata "0"
+      4000,  # Sources with conversion metadata "1"
+  ]
+  k = len(true_reports)
 
   # Simulate the API randomly flipping reports
-  noisy_reports = collections.defaultdict(int)
-  buckets = list(example_reports.keys())
-  noise_probability = .05
-  for bucket, count in example_reports.items():
+  randomized_response_rate = .1
+  noisy_reports = [0] * k
+  for i,count in enumerate(true_reports):
     for r in range(count):
-      if random.random() <= noise_probability:
-        new_bucket = random.choice(buckets)
+      if random.random() <= randomized_response_rate:
+        new_bucket = random.randint(0, k - 1)
         noisy_reports[new_bucket] += 1
       else:
-        noisy_reports[bucket] += 1
+        noisy_reports[i] += 1
 
-  corrected = corrected_buckets(noisy_reports, noise_probability)
+  corrected = estimate_true_values(noisy_reports, randomized_response_rate)
 
   column_names = ["Bucket", "True count", "Noisy count", "Corrected count"]
   print("{:<20}{:<20}{:<20}{:<20}".format(*column_names))
-  for bucket, count in sorted(corrected.items()):
-    print(f"{bucket:<20}{example_reports[bucket]:<20}{noisy_reports[bucket]:<20}{count:<20.2f}")
+  for i, count in enumerate(corrected):
+    print(f"{i:<20}{true_reports[i]:<20}{noisy_reports[i]:<20}{count:<20.2f}")
