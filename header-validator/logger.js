7fbe0c9c9e3263256f08e765a724be0495e796f3
maudnals <maud.nalpas@gmail.com>
2022-07-01 07:49:10 +0200

    Refactored/DRYed logger

--- header-validator/logger.js
+++ header-validator/logger.js
@@ -1,31 +1,63 @@
 import { formatIssue } from './validate-json.js';
 
-export function logValidation(header, validationFunction) {
+// Issue types
+const ERROR = 'ERROR';
+const WARNING = 'WARNING';
+
+// red
+const ERROR_COLOR = '\x1b[31m%s\x1b[0m';
+// yellow
+const WARNING_COLOR = '\x1b[33m%s\x1b[0m';
+// green
+const SUCCESS_COLOR = '\x1b[32m%s\x1b[0m';
+
+function logHeader(header) {
   console.log(`\n`);
   console.log(`----------------------------------------------`);
   console.log(header);
   console.log(`----------------------------------------------`);
+}
 
+function logSuccess() {
+  console.log(SUCCESS_COLOR, `✅ 0 ERRORS, 0 WARNINGS`);
+}
+
+function logIssues(issues, issuesType) {
+  let color;
+  let symbol;
+  if (issuesType === ERROR) {
+    color = ERROR_COLOR;
+    symbol = '❌';
+  } else if (issuesType === WARNING) {
+    color = WARNING_COLOR;
+    symbol = '⚠️';
+  }
+  const issuesCount = issues.length;
+  console.log(
+    `${symbol}  ${issuesCount} ${issuesType}${issuesCount > 1 ? 'S' : ''}:`
+  );
+  const issuesFormatted = issues.map((issue) => formatIssue(issue));
+  issuesFormatted.forEach((issueFormatted) =>
+    console.log(color, issueFormatted)
+  );
+}
+
+export function logValidation(header, validationFunction) {
+  // Log header
+  logHeader(header);
+
+  // Validate
   const { errors, warnings } = validationFunction(header);
 
+  // Log validation output
   if (errors.length === 0 && warnings.length === 0) {
-    console.log('\x1b[32m%s\x1b[0m', `✅ 0 ERRORS, 0 WARNINGS`);
+    logSuccess();
   }
-
   if (errors.length) {
-    console.log(`❌ ${errors.length} ERROR(S):`);
-    const errorsFormatted = errors.map((error) => formatIssue(error));
-    errorsFormatted.forEach((errorFormatted) =>
-      console.log('\x1b[31m%s\x1b[0m', errorFormatted)
-    );
+    logIssues(errors, ERROR);
   }
-
   if (warnings.length) {
-    console.log(`⚠️  ${warnings.length} WARNING(S):`);
-    const warningsFormatted = warnings.map((warning) => formatIssue(warning));
-    warningsFormatted.forEach((warningFormatted) =>
-      console.log('\x1b[33m%s\x1b[0m', warningFormatted)
-    );
+    logIssues(warnings, WARNING);
   }
 }
 
