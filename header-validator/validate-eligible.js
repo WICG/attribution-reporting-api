46fae8bbcb8f27f917b9b9043ec9f48b4be43fc3
Andrew Paseltiner <apaseltiner@google.com>
2022-08-08 11:51:37 -0400

    Add validator for Attribution-Reporting-Eligible header (#536)

---
+++ header-validator/validate-eligible.js
@@ -0,0 +1,41 @@
+import { parseDictionary } from 'structured-headers'
+
+export function validateEligible(str) {
+  const errors = []
+  const warnings = []
+
+  let dict
+  try {
+    dict = parseDictionary(str)
+  } catch (err) {
+    errors.push({ msg: err.toString() })
+    return { errors, warnings }
+  }
+
+  for (const [key, value] of dict) {
+    switch (key) {
+      case 'event-source':
+      case 'trigger':
+        break
+      case 'navigation-source':
+        warnings.push({
+          msg: 'may only be specified in browser-initiated requests',
+          path: [key],
+        })
+        break
+      default:
+        warnings.push({ msg: 'unknown dictionary key', path: [key] })
+        break
+    }
+
+    if (value[0] !== true) {
+      warnings.push({ msg: 'ignoring dictionary value', path: [key] })
+    }
+
+    if (value[1].size !== 0) {
+      warnings.push({ msg: 'ignoring parameters', path: [key] })
+    }
+  }
+
+  return { errors, warnings }
+}
