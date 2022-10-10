db1dc666056e6c732e73119df601891100911a6f
John Delaney <40465606+johnivdel@users.noreply.github.com>
2022-10-10 16:40:54 -0400

    Update data.source.js

--- header-validator/data.source.js
+++ header-validator/data.source.js
@@ -9,7 +9,6 @@ export const validSourceHeadersAsObjects = [
     priority: '4567898765678',
     debug_key: '9876786543456',
     expiry: '123',
-    aggregatable_expiry: '456',
     aggregation_keys: {
       campaignCounts: '0x159',
       geoValue: '0x5',
