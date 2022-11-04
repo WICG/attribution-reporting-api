2a827902d52c9ca860ed1e24cdc2ca11c1a772e3
John Delaney <40465606+johnivdel@users.noreply.github.com>
2022-11-04 12:06:11 -0400

    Update test.os.js

--- header-validator/test.os.js
+++ header-validator/test.os.js
@@ -3,42 +3,24 @@ import { logHeaderListValidation } from './logger.js'
 
 const osSourceTests = [
   // Valid
-  '"https://a.test/"; os-destination="b"; web-destination="https://c.test"',
-  '"http://localhost/"; os-destination="b"; web-destination="https://c.test"',
-  '"http://127.0.0.1/"; os-destination="b"; web-destination="https://c.test"',
+  '"https://a.test/"',
+  '"http://localhost/"',
+  '"http://127.0.0.1/"',
 
   // Warnings
-  '"https://a.test/"; os-destination="b"; web-destination="https://c.test"; x=1',
+  '"https://a.test/"; x=1',
 
   // Invalid header syntax
   '!',
 
   // Not a string
-  'x; os-destination="b"; web-destination="https://c.test"',
+  'x',
 
   // Invalid URL
-  '"a.test"; os-destination="b"; web-destination="https://c.test"',
+  '"a.test"',
 
   // Untrustworthy URL
-  '"http://a.test"; os-destination="b"; web-destination="https://c.test"',
-
-  // Missing os-destination
-  '"https://a.test/"; web-destination="https://c.test"',
-
-  // os-destination not a string
-  '"https://a.test/"; os-destination=b; web-destination="https://c.test"',
-
-  // Missing web-destination
-  '"https://a.test/"; os-destination="b"',
-
-  // web-destination not a string
-  '"http://localhost/"; os-destination="b"; web-destination=b',
-
-  // web-destination invalid URL
-  '"https://a.test/"; os-destination="b"; web-destination="c.test"',
-
-  // web-destination untrustworthy URL
-  '"https://a.test/"; os-destination="b"; web-destination="http://c.test"',
+  '"http://a.test"',
 ]
 
 logHeaderListValidation(osSourceTests, validateRegisterOsSource)
