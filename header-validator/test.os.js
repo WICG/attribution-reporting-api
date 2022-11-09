fedac4d8a596866ce5486a9b7f9ea88284515c7e
Andrew Paseltiner <apaseltiner@google.com>
2022-11-09 16:07:49 -0500

    Use assertions in OS-registration header validation instead of log output (#611)

--- header-validator/test.os.js
+++ header-validator/test.os.js
@@ -1,50 +1,63 @@
+import assert from 'node:assert/strict'
 import { validateRegisterOsSource, validateRegisterOsTrigger } from './validate-os.js'
-import { logHeaderListValidation } from './logger.js'
 
-const osSourceTests = [
+const tests = [
   // Valid
-  '"https://a.test/"',
-  '"http://localhost/"',
-  '"http://127.0.0.1/"',
+  {input: '"https://a.test/"'},
+  {input: '"http://localhost/"'},
+  {input: '"http://127.0.0.1/"'},
 
   // Warnings
-  '"https://a.test/"; x=1',
+  {
+    input: '"https://a.test/"; x=1',
+    warnings: [{
+      path: ['x'],
+      msg: 'unknown parameter',
+    }],
+  },
 
   // Invalid header syntax
-  '!',
+  {
+    input: '!',
+    errors: [{msg: 'Error: Parse error: Unexpected input at offset 0'}],
+  },
 
   // Not a string
-  'x',
+  {
+    input: 'x',
+    errors: [{
+      path: [],
+      msg: 'must be a string',
+    }],
+  },
 
   // Invalid URL
-  '"a.test"',
+  {
+    input: '"a.test"',
+    errors: [{
+      path: [],
+      msg: 'must contain a valid URL',
+    }],
+  },
 
   // Untrustworthy URL
-  '"http://a.test"',
+  {
+    input: '"http://a.test"',
+    errors: [{
+      path: [],
+      msg: 'must contain a potentially trustworthy URL',
+    }],
+  },
 ]
 
-logHeaderListValidation(osSourceTests, validateRegisterOsSource)
+tests.forEach(test => {
+  [
+      validateRegisterOsSource,
+      validateRegisterOsTrigger,
+  ].forEach(validate => {
+    const { errors, warnings } = validate(test.input);
 
-const osTriggerTests = [
-  // Valid
-  '"https://a.test/"',
-  '"http://localhost/"',
-  '"http://127.0.0.1/"',
-
-  // Warnings
-  '"https://a.test/"; x=1',
-
-  // Invalid header syntax
-  '!',
-
-  // Not a string
-  'x',
-
-  // Invalid URL
-  '"a.test"',
-
-  // Untrustworthy URL
-  '"http://a.test/"',
-]
-
-logHeaderListValidation(osTriggerTests, validateRegisterOsTrigger)
+    assert.deepEqual(errors, test.errors || [], test.input);
+    assert.deepEqual(warnings, test.warnings || [], test.input);
+  });
+});
