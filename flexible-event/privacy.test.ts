ab43f8c989cf881ffd7a7f71801b98d649ed164a
Andrew Paseltiner <apaseltiner@google.com>
2023-09-08 11:46:01 -0400

    Lint flexible-event package (#998)

--- flexible-event/privacy.test.ts
+++ flexible-event/privacy.test.ts
@@ -1,94 +1,109 @@
-import { flipProbabilityDp, maxInformationGain, binaryEntropy, DefaultConfig, SourceType } from './privacy'
+import {
+  flipProbabilityDp,
+  maxInformationGain,
+  binaryEntropy,
+  DefaultConfig,
+  SourceType,
+} from './privacy'
 import { strict as assert } from 'assert'
 
 const flipProbabilityTests = [
-    {
-        numStates: 2,
-        epsilon: Math.log(3),
-        expected: .5
-    },
-    {
-        numStates: 3,
-        epsilon: Math.log(3),
-        expected: .6
-    },
-    {
-        numStates: 2925,
-        epsilon: 14,
-        expected: 0.0024263221679834087
-    },
-    {
-        numStates: 3,
-        epsilon: 14,
-        expected: 0.000002494582008677539
-    },
+  {
+    numStates: 2,
+    epsilon: Math.log(3),
+    expected: 0.5,
+  },
+  {
+    numStates: 3,
+    epsilon: Math.log(3),
+    expected: 0.6,
+  },
+  {
+    numStates: 2925,
+    epsilon: 14,
+    expected: 0.0024263221679834087,
+  },
+  {
+    numStates: 3,
+    epsilon: 14,
+    expected: 0.000002494582008677539,
+  },
 ]
 
-flipProbabilityTests.forEach(test => {
-    const flipProb = flipProbabilityDp(test.numStates, test.epsilon)
-    assert.deepStrictEqual(flipProb, test.expected)
+flipProbabilityTests.forEach((test) => {
+  const flipProb = flipProbabilityDp(test.numStates, test.epsilon)
+  assert.deepStrictEqual(flipProb, test.expected)
 })
 
 const infoGainTests = [
-    {
-        numStates: 2,
-        epsilon: Infinity,
-        expected: 1
-    },
-    {
-        numStates: 1024,
-        epsilon: Infinity,
-        expected: Math.log2(1024)
-    },
-    {
-        numStates: 3,
-        epsilon: Infinity,
-        expected: Math.log2(3)
-    },
-    {
-        numStates: 2,
-        epsilon: Math.log(3),
-        expected: 0.18872187554086717
-    },
-    {
-        numStates: 2925,
-        epsilon: 14,
-        expected: 11.461727965384876
-    },
-    {
-        numStates: 3,
-        epsilon: 14,
-        expected: 1.584926511508231
-    },
+  {
+    numStates: 2,
+    epsilon: Infinity,
+    expected: 1,
+  },
+  {
+    numStates: 1024,
+    epsilon: Infinity,
+    expected: Math.log2(1024),
+  },
+  {
+    numStates: 3,
+    epsilon: Infinity,
+    expected: Math.log2(3),
+  },
+  {
+    numStates: 2,
+    epsilon: Math.log(3),
+    expected: 0.18872187554086717,
+  },
+  {
+    numStates: 2925,
+    epsilon: 14,
+    expected: 11.461727965384876,
+  },
+  {
+    numStates: 3,
+    epsilon: 14,
+    expected: 1.584926511508231,
+  },
 ]
 
-infoGainTests.forEach(test => {
-    const infoGain = maxInformationGain(test.numStates, test.epsilon)
-    assert.deepStrictEqual(infoGain, test.expected)
+infoGainTests.forEach((test) => {
+  const infoGain = maxInformationGain(test.numStates, test.epsilon)
+  assert.deepStrictEqual(infoGain, test.expected)
 })
 
 const binaryEntropyTests = [
-    { x: 0, expected: 0 },
-    { x: .5, expected: 1 },
-    { x: 1, expected: 0 },
-    { x: .01, expected: 0.08079313589591118 },
-    { x: .99, expected: 0.08079313589591124 },
+  { x: 0, expected: 0 },
+  { x: 0.5, expected: 1 },
+  { x: 1, expected: 0 },
+  { x: 0.01, expected: 0.08079313589591118 },
+  { x: 0.99, expected: 0.08079313589591124 },
 ]
 
-binaryEntropyTests.forEach(test => {
-    assert.deepStrictEqual(binaryEntropy(test.x), test.expected)
+binaryEntropyTests.forEach((test) => {
+  assert.deepStrictEqual(binaryEntropy(test.x), test.expected)
 })
 
-assert.deepStrictEqual(DefaultConfig[SourceType.Navigation].computeConfigData(14, SourceType.Navigation), {
+assert.deepStrictEqual(
+  DefaultConfig[SourceType.Navigation].computeConfigData(
+    14,
+    SourceType.Navigation
+  ),
+  {
     numStates: 2925,
     infoGain: 11.461727965384876,
     flipProb: 0.0024263221679834087,
-    excessive: undefined
-})
+    excessive: undefined,
+  }
+)
 
-assert.deepStrictEqual(DefaultConfig[SourceType.Event].computeConfigData(14, SourceType.Event), {
+assert.deepStrictEqual(
+  DefaultConfig[SourceType.Event].computeConfigData(14, SourceType.Event),
+  {
     numStates: 3,
     infoGain: 1.584926511508231,
     flipProb: 0.000002494582008677539,
-    excessive: undefined
-})
+    excessive: undefined,
+  }
+)
--- flexible-event/privacy.ts
+++ flexible-event/privacy.ts
@@ -6,42 +6,47 @@ export enum SourceType {
 }
 
 export type ExcessiveInfoGainData = {
-  infoGainDefault: number,
-  newEps: number,
-  newFlipProb: number,
+  infoGainDefault: number
+  newEps: number
+  newFlipProb: number
 }
 
 export type ConfigData = {
-  numStates: number,
-  infoGain: number,
-  flipProb: number,
-  excessive?: ExcessiveInfoGainData,
+  numStates: number
+  infoGain: number
+  flipProb: number
+  excessive?: ExcessiveInfoGainData
 }
 
 export class PerTriggerDataConfig {
   constructor(
     readonly numWindows: number,
-    readonly numSummaryBuckets: number) {
-      if (this.numWindows <= 0) {
-        throw 'numWindows must be > 0'
-      }
-      if (this.numSummaryBuckets <= 0) {
-        throw 'numSummaryBuckets must be > 0'
-      }
+    readonly numSummaryBuckets: number
+  ) {
+    if (this.numWindows <= 0) {
+      throw 'numWindows must be > 0'
     }
+    if (this.numSummaryBuckets <= 0) {
+      throw 'numSummaryBuckets must be > 0'
+    }
+  }
 }
 
 export class Config {
   constructor(
     readonly maxEventLevelReports: number,
-    readonly perTriggerDataConfigs: ReadonlyArray<PerTriggerDataConfig>) {
-      if (this.maxEventLevelReports <= 0 || !Number.isInteger(this.maxEventLevelReports)) {
-        throw 'maxEventLevelReports must be an integer > 0'
-      }
-      if (this.perTriggerDataConfigs.length === 0) {
-        throw 'perTriggerDataConfigs must be non-empty'
-      }
+    readonly perTriggerDataConfigs: ReadonlyArray<PerTriggerDataConfig>
+  ) {
+    if (
+      this.maxEventLevelReports <= 0 ||
+      !Number.isInteger(this.maxEventLevelReports)
+    ) {
+      throw 'maxEventLevelReports must be an integer > 0'
     }
+    if (this.perTriggerDataConfigs.length === 0) {
+      throw 'perTriggerDataConfigs must be non-empty'
+    }
+  }
 
   private numFlexibleStates(): number {
     // Let B be the trigger data cardinality.
@@ -50,27 +55,40 @@ export class Config {
     // 1. A[C, w_1, ..., w_B, c_1, ... , c_B] = 1 if B = 0
     // 2. A[C, w_1, ..., w_B, c_1, ... , c_B] = A[C, w_1, ..., w_{B-1}, c_1, ... , c_{B-1}] if w_B = 0
     // 3. A[C, w_1, ..., w_B, c_1, ... , c_B] = sum(A[C - j, w_1, ..., w_B - 1, c_1, ... , c_B - j], j from 0 to min(c_B, C)) otherwise
-    const helper = memoize((totalCap: number, index: number, w: number, c: number): number => {
-      if (index == 0 && w == 0) {
-        return 1
-      }
+    const helper = memoize(
+      (totalCap: number, index: number, w: number, c: number): number => {
+        if (index == 0 && w == 0) {
+          return 1
+        }
 
-      if (w == 0) {
-        const triggerConfig = this.perTriggerDataConfigs[index - 1]
-        return helper(totalCap, index - 1, triggerConfig.numWindows, triggerConfig.numSummaryBuckets)
-      }
+        if (w == 0) {
+          const triggerConfig = this.perTriggerDataConfigs[index - 1]
+          return helper(
+            totalCap,
+            index - 1,
+            triggerConfig.numWindows,
+            triggerConfig.numSummaryBuckets
+          )
+        }
 
-      let sum = 0
-      const end = Math.min(c, totalCap)
-      for (let i = 0; i <= end; i++) {
-        sum += helper(totalCap - i, index, w - 1, c - i)
+        let sum = 0
+        const end = Math.min(c, totalCap)
+        for (let i = 0; i <= end; i++) {
+          sum += helper(totalCap - i, index, w - 1, c - i)
+        }
+        return sum
       }
-      return sum
-    })
+    )
 
-    const lastConfig = this.perTriggerDataConfigs[this.perTriggerDataConfigs.length - 1]
+    const lastConfig =
+      this.perTriggerDataConfigs[this.perTriggerDataConfigs.length - 1]
     const dataCardinality = this.perTriggerDataConfigs.length
-    return helper(this.maxEventLevelReports, dataCardinality - 1, lastConfig.numWindows, lastConfig.numSummaryBuckets)
+    return helper(
+      this.maxEventLevelReports,
+      dataCardinality - 1,
+      lastConfig.numWindows,
+      lastConfig.numSummaryBuckets
+    )
   }
 
   computeConfigData(epsilon: number, sourceType: SourceType): ConfigData {
@@ -79,27 +97,40 @@ export class Config {
     const flipProb = flipProbabilityDp(numStates, epsilon)
 
     let excessive
-    const infoGainDefault = (sourceType === 'event' ? 6.5 : maxInformationGain(
-          DefaultConfig[sourceType].numFlexibleStates(), epsilon))
+    const infoGainDefault =
+      sourceType === 'event'
+        ? 6.5
+        : maxInformationGain(
+            DefaultConfig[sourceType].numFlexibleStates(),
+            epsilon
+          )
 
     if (infoGain > infoGainDefault) {
-        const newEps = epsilonToBoundInfoGainAndDp(numStates, infoGainDefault, epsilon)
-        const newFlipProb = flipProbabilityDp(numStates, newEps)
-        excessive= {infoGainDefault, newEps, newFlipProb}
+      const newEps = epsilonToBoundInfoGainAndDp(
+        numStates,
+        infoGainDefault,
+        epsilon
+      )
+      const newFlipProb = flipProbabilityDp(numStates, newEps)
+      excessive = { infoGainDefault, newEps, newFlipProb }
     }
 
-    return {numStates, infoGain, flipProb, excessive}
+    return { numStates, infoGain, flipProb, excessive }
   }
 }
 
 export const DefaultConfig: Readonly<Record<SourceType, Config>> = {
   [SourceType.Navigation]: new Config(
-    /*maxEventLevelReports=*/3,
-    new Array(8).fill(new PerTriggerDataConfig(/*numWindows=*/3, /*numSummaryBuckets=*/3)),
+    /*maxEventLevelReports=*/ 3,
+    new Array(8).fill(
+      new PerTriggerDataConfig(/*numWindows=*/ 3, /*numSummaryBuckets=*/ 3)
+    )
   ),
   [SourceType.Event]: new Config(
-    /*maxEventLevelReports=*/1,
-    new Array(2).fill(new PerTriggerDataConfig(/*numWindows=*/1, /*numSummaryBuckets=*/1)),
+    /*maxEventLevelReports=*/ 1,
+    new Array(2).fill(
+      new PerTriggerDataConfig(/*numWindows=*/ 1, /*numSummaryBuckets=*/ 1)
+    )
   ),
 }
 
@@ -131,23 +162,33 @@ export function flipProbabilityDp(numStates: number, epsilon: number): number {
  *   the channel. In the special case of the q-ary symmetric channel, a
  *   closed-form expression is known, which we use here.
  */
-function capacityQarySymmetricChannel(log2q: number, flipProbability: number): number {
-  return log2q - binaryEntropy(flipProbability) - flipProbability * Math.log2(Math.pow(2, log2q) - 1)
+function capacityQarySymmetricChannel(
+  log2q: number,
+  flipProbability: number
+): number {
+  return (
+    log2q -
+    binaryEntropy(flipProbability) -
+    flipProbability * Math.log2(Math.pow(2, log2q) - 1)
+  )
 }
 
 export function maxInformationGain(numStates: number, epsilon: number): number {
   const flipProb = flipProbabilityDp(numStates, epsilon)
-  return capacityQarySymmetricChannel(Math.log2(numStates),
-      flipProb * (numStates - 1) / numStates)
+  return capacityQarySymmetricChannel(
+    Math.log2(numStates),
+    (flipProb * (numStates - 1)) / numStates
+  )
 }
 
 // Returns the effective epsilon needed to satisfy an information gain bound
 // given a number of output states in the q-ary symmetric channel.
 function epsilonToBoundInfoGainAndDp(
-    numStates: number,
-    infoGainUpperBound: number,
-    epsilonUpperBound: number,
-    tolerance: number = 0.00001): number {
+  numStates: number,
+  infoGainUpperBound: number,
+  epsilonUpperBound: number,
+  tolerance: number = 0.00001
+): number {
   // Just perform a simple binary search over values of epsilon.
   let epsLow = 0
   let epsHigh = epsilonUpperBound
