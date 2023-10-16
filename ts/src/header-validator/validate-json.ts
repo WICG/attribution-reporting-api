d4c010c943af67f15f7dbdf64b478a56a089e659
Nan Lin <linnan@chromium.org>
2023-10-16 09:46:50 -0400

    lint

--- ts/src/header-validator/validate-json.ts
+++ ts/src/header-validator/validate-json.ts
@@ -1105,9 +1105,7 @@ function source(ctx: Context, j: Json): Maybe<Source> {
 
 function sourceKeys(ctx: Context, j: Json): Maybe<Set<string>> {
   return set(ctx, j, (ctx, j) =>
-    string(ctx, j).filter((s) =>
-      aggregationKeyIdentifierLength(ctx, s)
-    )
+    string(ctx, j).filter((s) => aggregationKeyIdentifierLength(ctx, s))
   )
 }
 
