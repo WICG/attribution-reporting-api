3e68f98f2552c0edffb3f40b4d636f45de0f5541
Nan Lin <linnan@chromium.org>
2024-02-07 18:27:31 -0500

    Spec change for supporting preferred platform for cross app attribution

---
+++ ts/src/header-validator/validate-info.ts
@@ -0,0 +1,41 @@
+import { Context, ValidationResult } from './context'
+import { parseDictionary } from 'structured-headers'
+import { Token } from 'structured-headers'
+
+export function validateInfo(str: string): ValidationResult {
+  const ctx = new Context()
+
+  let dict
+  try {
+    dict = parseDictionary(str)
+  } catch (err) {
+    const msg = err instanceof Error ? err.toString() : 'unknown error'
+    return ctx.finish(msg)
+  }
+
+  for (const [key, value] of dict) {
+    ctx.scope(key, () => {
+      switch (key) {
+        case 'preferred-platform':
+          if (typeof value[0] !== 'object' || value[0].constructor !== Token) {
+            ctx.error('must be a token')
+            return
+          }
+          const token = value[0].toString()
+          if (token !== 'os' && token !== 'web') {
+            ctx.error('must be one of the following (case-sensitive): os, web')
+            return
+          }
+          if (value[1].size !== 0) {
+            ctx.warning('ignoring parameters')
+          }
+          break
+        default:
+          ctx.warning('unknown dictionary key')
+          break
+      }
+    })
+  }
+
+  return ctx.finish()
+}
